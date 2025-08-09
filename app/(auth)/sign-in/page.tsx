f1820aebaab932f929b054a4eed63522a058596b
Rahat Talukder <rahatulab@gmail.com>
2025-08-10 00:56:07 +0600

    name update for new asset

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -16,7 +16,7 @@ const SignIn = () => {
             width={32}
             height={32}
           />
-          <h1>SnapCast</h1>
+          <h1>Scroo</h1>
         </Link>
 
         <div className="description">
